61b77309eac0616b7c460eb042bca966118d9597
Stephan Lefort <lefortsge@gmail.com>
2020-01-25 11:14:32 -0800

    Testing changes for build stage

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy app")
+    return jsonify("Healthy appy")
 
 
 @APP.route('/auth', methods=['POST'])
